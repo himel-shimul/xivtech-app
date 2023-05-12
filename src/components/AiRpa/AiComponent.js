3642d60440dbb888ef22749d1317f28fadb6053a
himel-shimul <himelshimul00@gmail.com>
2023-05-12 15:57:08 +0600

    digitai component responsive

--- src/components/AiRpa/AiComponent.js
+++ src/components/AiRpa/AiComponent.js
@@ -13,12 +13,12 @@ const AiComponent = () => {
         <AiRightSide></AiRightSide>
       </div>
       </div>
-      <div className="lg:hidden">
+      <div className="lg:hidden p-2 mb-8">
         <h2 className="text-[38px] leading-relaxed font-serif">
           AI + RPA is what we do
         </h2>
         <img className='w-full mt-8 h-auto my-auto' src={img} alt="" />
-        <h2 className="text-xl leading-relaxed font-serif">
+        <h2 className="text-xl leading-relaxed font-serif mt-3">
           Future-Proof your business. Drive efficiency, profitability{" "}
           and deliver on customer experience
         </h2>
--- src/components/digCom/DigitalComponents.js
+++ src/components/digCom/DigitalComponents.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import DigLeftSide from './DigLeftSide';
 import DigRightSide from './DigRightSide';
+import { BsArrowRight } from 'react-icons/bs';
 
 const DigitalComponents = () => {
     return (
-        <div className='flex w-[60%] my-4 mx-auto'>
+        <>
+        <div className='hidden lg:block '>
+            <div className=' flex w-[60%] my-4 mx-auto'>
             <DigLeftSide></DigLeftSide>
             <DigRightSide></DigRightSide>
+            </div>
         </div>
+        <div className="lg:hidden p-2 mb-8">
+        <h2 className="text-[36px] font-serif">
+      Make Bolder Choices
+      </h2>
+      <img className='w-full mt-8 h-auto my-auto' src="https://www.xivtech.io./p1.png" alt="" />
+
+      <h2 className="text-xl leading-relaxed font-serif mt-3">
+      Digital focused strategies to realize market-changing ideas
+      </h2>
+      <h2 className='text-xl text-blue-500 cursor-pointer font-bold font-serif mt-8'>Build Better Apps <BsArrowRight className='inline text-black hover:translate-x-3 duration-300 text-3xl mx-6'></BsArrowRight></h2>
+        </div>
+        </>
     );
 };
 
